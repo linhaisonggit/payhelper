4271c6acf73941e981b8edc7f419702b49dd3023
llinhaisong <418942861@qq.com>
2018-09-11 10:59:04 +0800

    11

---
+++ src/main/java/com/payhelper/App.java
@@ -0,0 +1,13 @@
+package com.payhelper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
